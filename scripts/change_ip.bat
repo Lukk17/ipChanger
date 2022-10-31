9657289908aa18dbc11025d840924464604065c4
Łukasz Sarna <luksarna@gmail.com>
2022-10-31 21:48:18 +0100

    add metrics for address change

--- scripts/change_ip.bat
+++ scripts/change_ip.bat
@@ -6,7 +6,8 @@ SET IP_ADDR=%1
 SET SUBNET=%2
 SET GATEWAY=%3
 
-netsh interface ip set address name=%INTERFACE% static %IP_ADDR% %SUBNET% %GATEWAY%
+REM "1" at end is for metric (better leave as is)
+netsh interface ip set address name=%INTERFACE% static %IP_ADDR% %SUBNET% %GATEWAY% 1
+
 
 endlocal
-
